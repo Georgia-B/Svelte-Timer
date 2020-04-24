96eca172276dc2a83aa6dfe855026ec9882c9801
Georgia Barnett <gbarnett.nzl@gmail.com>
2020-04-25 10:53:22 +1200

    Add cypress test for button state

--- cypress/integration/countdown.spec.js
+++ cypress/integration/countdown.spec.js
@@ -12,4 +12,29 @@ context('Countdown', () => {
         cy.get('input[id*="seconds"]').should('have.attr', 'placeholder', '00');
         cy.get('input[id*="seconds"]').should('have.value', '');
     });
+    it('should show correct buttons', () => {
+        cy.get('button[class*="button-start"]').should('exist');
+        cy.get('button[class*="button-clear"]').should('exist');
+        cy.get('button[class*="button-pause"]').should('not.exist');
+
+        cy.get('input[id*="seconds"]')
+            .type(3).should('have.value', '3');
+        cy.get('button[class*="button-start"]').click();
+
+        cy.get('button[class*="button-start"]').should('not.exist');
+        cy.get('button[class*="button-clear"]').should('exist');
+        cy.get('button[class*="button-pause"]').should('exist');
+
+        cy.wait(3000);
+
+        cy.get('button[class*="button-start"]').should('not.exist');
+        cy.get('button[class*="button-clear"]').should('exist');
+        cy.get('button[class*="button-pause"]').should('not.exist');
+
+        cy.get('button[class*="button-clear"]').click();
+
+        cy.get('button[class*="button-start"]').should('exist');
+        cy.get('button[class*="button-clear"]').should('exist');
+        cy.get('button[class*="button-pause"]').should('not.exist');
+    });
 });
